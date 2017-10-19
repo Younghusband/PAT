e9fd548cb895a8eb0178b4bb7196f8d37d1a6636
vermouth.yf <vermouth.yf@gmail.com>
2017-10-19 11:41:50 +0800

    init my PAT Project @master

---
+++ src/yangfan/yi/Question1001.java
@@ -0,0 +1,23 @@
+package yangfan.yi;
+
+/*********************************************
+ * Question1001.java
+ * Author: Vermouth.yf
+ * Created on: 2017-10-19
+ *
+ * Details:
+ ********************************************/
+
+public class Question1001 {
+
+	/**
+	 * Author: Vermouth.yf 
+	 * Date: 2017-10-19 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
